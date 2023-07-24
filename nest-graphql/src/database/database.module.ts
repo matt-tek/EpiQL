3680b24cd3401d98c04a1cc78f895d1a7bd17a66
fedora <mathieu.muty@epitech.eu>
2023-07-24 17:57:44 +0200

    add database connection

--- nest-graphql/src/database/database.module.ts
+++ nest-graphql/src/database/database.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseService } from './database.service';
 import { DatabaseResolver } from './database.resolver';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root-password',
+      database: 'db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   providers: [DatabaseResolver, DatabaseService],
 })
 export class DatabaseModule {}
